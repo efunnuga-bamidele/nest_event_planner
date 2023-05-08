68bd46b1de5aa28457ee8df8653832bba189207c
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2023-05-08 07:46:46 +0100

    Create a Data Transfer Object (Dto) for the controller

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,20 +1,33 @@
-import { Controller, Get, Post, Patch, Delete } from "@nestjs/common";
+import { Controller, Get, Post, Patch, Delete, Param, Body, HttpCode } from "@nestjs/common";
+import { CreateEventDto } from "./create-event.dto";
 
 @Controller('/events') //part prefix
 export class EventsController {
 
     @Get()
-    findAll() { }
+    findAll() { 
+        return [
+            {id:1, name: "First Event"},
+            {id:2, name: "Second Event"},
+        ];
+    }
 
-    @Get()
-    findOne() { }
+    @Get(':id')
+    findOne(@Param('id') id: number) {
+        return {id:1, name: "First Event"};
+    }
 
     @Post()
-    create() { }
+    create(@Body() input: CreateEventDto) {
+        return input;
+    }
 
-    @Patch()  //@Put update all resources @Patch update specific property of the resources
-    update() { }
+    @Patch(':id')  //@Put update all resources @Patch update specific property of the resources
+    update(@Param('id') id: number, @Body() input) { 
+        return input;
+    }
 
-    @Delete()
-    remove() { }
+    @Delete(':id')
+    @HttpCode(204)
+    remove(@Param('id') id: number) { }
 }
